0aabf06c231c188df3a11997a61a72e67f809374
xinhuixu <x.xu.1102@gmail.com>
2017-02-26 15:44:01 -0500

    dvd!

--- anime.js
+++ anime.js
@@ -5,6 +5,7 @@ var c_w = $(window).width();
 c.height = c_h;
 c.width = c_w; //fullscreen canv
 
+ctx.font = "3em Arial";
 var rid = 0;
 
 var b_circle = document.getElementById("b_circle");
@@ -25,6 +26,11 @@ var hypnosis = function(e) {
 	reset();
 	ctx.beginPath();
 	ctx.arc(0.5*c_w, 0.5*c_h, r, 0, 2*Math.PI);
+
+	if (r % 5 == 0) {
+	    var color = 'rgb(' + Math.floor(Math.random() * 256) + ',' +Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+	    ctx.fillStyle = color;
+	}
 	ctx.fill()
 
 	if (r >= 0 && r <= 0.15*c_w) {
@@ -47,7 +53,33 @@ var hypnosis = function(e) {
 }
 
 var dvd = function(e) {
-    console.log("dvd");
+    window.cancelAnimationFrame(rid);
+    var r = 200;    
+    var x = Math.floor(Math.random() * (c_w - r));
+    var y = Math.floor(Math.random() * (c_h - r/2));
+    var dx = 1.5;
+    var dy = 1.5;
+    var drift = function() {
+	reset();
+	ctx.fillStyle = "#83F52C";
+	ctx.fillRect(x, y, r, r/2);
+	
+	ctx.fillStyle = "#000000";
+	ctx.fillText("DVD", x+50, y+65);
+
+	//boing
+	if (x < 0 || x > c_w-r){
+	    dx = -1 * dx;
+	}
+	//boing
+	if (y < 0 || y > c_h-(r/2)) {
+	    dy = -1 * dy;
+	}
+	x += dx;
+	y += dy;
+	rid = window.requestAnimationFrame(drift);
+    }
+    drift();
 }
 
 var stop = function(e) {
